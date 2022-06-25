8006e04fbe8a03d19bce4ab437f41128eaa9d01c
RickyRaMx_c149 <ricardo.rmeza@live.com>
2022-06-24 20:55:17 -0500

    🎉 install vite and configure tailwindcss

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header(){
+
+    return(
+        <header className="grid">
+            <h1 className="text-4xl font-bold uppercase">Desde header</h1>
+
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ListadoPacientes = () => {
+    return (
+        <div>
+            <h2>Listado pacientes</h2>
+        </div>
+    )
+}
+
+    export default ListadoPacientes
+    
