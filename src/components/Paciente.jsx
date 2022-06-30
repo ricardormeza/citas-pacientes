6f57fb1a5a1ccaf2eaf67820d17500710326b9ca
RickyRaMx_c149 <ricardo.rmeza@live.com>
2022-06-29 19:22:20 -0500

    💄 listado pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Paciente = () => {
+  return (
+    <div className='m-3 bg-white shadow-md p-10 rounded-xl'>
+                {/* NOMBRE DE MASCOTA */}
+                <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre:{' '}
+                    <span className='font-normal normal-case'>Hook</span>
+                </p>
+                {/* NOMBRE DE PROPIETARIO */}
+                <p className='font-bold mb-3 text-gray-700 uppercase'>PROPIETARIO:{' '}
+                    <span className='font-normal normal-case'>Rick</span>
+                </p>
+                {/* EMAIL */}
+                <p className='font-bold mb-3 text-gray-700 uppercase'>Correo electrónico:{' '}
+                    <span className='font-normal normal-case'>correo@correo.com</span>
+                </p>
+                {/* FECHA ALTA */}
+                <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha Alta:{' '}
+                    <span className='font-normal normal-case'>10 Diciembre 2022</span>
+                </p>
+                {/* SINTOMAS */}
+                <p className='font-bold mb-3 text-gray-700 uppercase'>Sintomas:{' '}
+                    <span className='font-normal normal-case'>Lorem ipsum dolor sit amet.</span>
+                </p>
+    </div>
+  )
+}
+
+export default Paciente
